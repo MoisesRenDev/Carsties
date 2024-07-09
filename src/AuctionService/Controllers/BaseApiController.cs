f7e52575032369272820e44f99fcaff23ec78715
Moisés Rentería Gallegos <mrendev@outlook.com>
2024-07-09 15:48:05 -0600

    Section 3 endeed

---
+++ src/AuctionService/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
